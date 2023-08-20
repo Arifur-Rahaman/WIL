ca6299b7ed2fbf727cc4fb2e1e0505a4ece0a58d
Arifur-Rahaman <arifur.rahaman.siflu@gmail.com>
2023-08-20 13:09:57 +0600

    Updated use memo example

--- React/src/pages/UseMemo.jsx
+++ React/src/pages/UseMemo.jsx
@@ -2,26 +2,24 @@ import { useMemo, useState } from "react";
 
 const UseMemo = () => {
     const [color, setColor] = useState('red')
-    const [limit] = useState(1000000000)
+    const [limit, setLimit] = useState(1000000000)
     const changeColor = () => {
         setColor(prev => prev === 'red' ? 'black' : 'red')
     }
-    let count
-    useMemo(() => {
-        for (let i = 0; i < limit; i++) {
-            if(count){
-                count++
-            }
-            else{ 
-                count = 0
-            }
+    let count = useMemo(() => {
+        let sum = i
+        for (var i = 0; i < limit; i++) {
+            sum = i
         }
+        return sum
     }, [limit])
+
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh', background: `${color}` }}>
             <div>
                 <p style={{ color: 'white' }}>Count: {count}</p>
                 <button onClick={changeColor}>Change Color</button>
+                <button onClick={()=>setLimit(prev=>prev-1)}>Decrease Limit</button>
             </div>
         </div>
     );
