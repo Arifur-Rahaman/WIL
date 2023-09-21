726f384b826e9ae828014b126c9f46e2f85b41d1
Arifur-Rahaman <arifur.rahaman.siflu@gmail.com>
2023-09-21 15:54:13 +0600

    Maximum char in a string problem with solution

---
+++ Problem/compareObject/compareObject.js
@@ -0,0 +1,4 @@
+const arr1 = [2, 2]
+const arr2 = [2, 2]
+
+console.log(JSON.stringify(arr1) === JSON.stringify(arr2))
---
+++ Problem/maximumCharInString/maximumCharInString.js
@@ -0,0 +1,24 @@
+let str= 'asdfgggg kkdd'
+
+const freq = {}
+let maxChar= ''
+let charCount = 0
+
+for(let i=0; i<str.length; i++){
+    let currentElement = str[i]
+    if(freq[currentElement]){
+        freq[currentElement] = freq[currentElement] + 1
+    }
+    else{
+        freq[currentElement] = 1
+    }
+}
+
+for(let char in freq){
+    if(freq[char]>charCount){
+        charCount = freq[char]
+        maxChar = char
+    }
+}
+
+console.log(maxChar, charCount)
